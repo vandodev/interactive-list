7b0e9cb9dc7f2c8e55ed0b3d4c5ea27a87e5dc7e
vandodev <vandoaparecido@hotmail.com>
2024-01-08 21:20:46 -0300

    :straight_ruler: :notebook: :pencil2: Instalando e configurando dependências

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
 import { StatusBar } from 'react-native';
-import {List} from './src/screens/List'
+import {List} from './src/screens/List';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+
 
 export default function App() {
   return (
-    <>
+    <GestureHandlerRootView style={{flex:1}}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <List />
-    </>
+    </GestureHandlerRootView>
   );
 }
 
